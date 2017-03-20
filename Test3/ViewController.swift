677c7c695fa6656119af140e33bbb997afb2c5ef
Chloe Hutchings <i7243787@bournemouth.ac.uk>
2017-03-20 15:14:36 +0000

    GPX
    Added GPX file

--- Test3/ViewController.swift
+++ Test3/ViewController.swift
@@ -7,19 +7,63 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    @IBOutlet var mapView: MKMapView!
+    
+    
+    let manager = CLLocationManager()
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation])
+    
+    {
+        let location = locations [0]
+        
+        
+        let span:MKCoordinateSpan = MKCoordinateSpanMake(0.01, 0.01)
+        
+        let myLocation:CLLocationCoordinate2D = CLLocationCoordinate2DMake(location.coordinate.latitude, location.coordinate.longitude)
+        
+        let region:MKCoordinateRegion = MKCoordinateRegionMake(myLocation, span)
+        
+        mapView.setRegion(region, animated: true)
+        
+        print(location.speed)
+        
+        self.mapView.showsUserLocation = true 
+        
+        
     }
+    
+    
+    override func viewDidLoad() {
+        
+        
+        super.viewDidLoad()
+        
+        manager.delegate = self
+        manager.desiredAccuracy = kCLLocationAccuracyBest
+        manager.requestWhenInUseAuthorization()
+        manager.startUpdatingLocation()
+        
+        
+        
+        
+    
+        }
+        
+
+    
+    
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
 
-
+    
 }
-
