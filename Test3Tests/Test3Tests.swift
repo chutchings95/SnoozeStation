18304ab8d5fd0d6e8877815be912073f8e96cd3b
Chloe Hutchings <i7243787@bournemouth.ac.uk>
2017-03-20 13:13:37 +0000

    Initial Commit

---
+++ Test3Tests/Test3Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Test3Tests.swift
+//  Test3Tests
+//
+//  Created by Chloe Hutchings on 20/03/2017.
+//  Copyright © 2017 Chloe Hutchings. All rights reserved.
+//
+
+import XCTest
+@testable import Test3
+
+class Test3Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
