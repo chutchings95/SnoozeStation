0a45a338a27a764f9ea7c1086b2a99f3f7a05b12
Chloe Hutchings <i7243787@bournemouth.ac.uk>
2017-05-27 17:43:00 +0100

    Transition Changes

--- Test3/BounceButton.swift
+++ Test3/BounceButton.swift
@@ -4,9 +4,9 @@ import UIKit
 class BounceButton: UIButton {
 
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        self.transform = CGAffineTransform(scaleX: 1.1, y: 1.1)
+        self.transform = CGAffineTransform(scaleX: 1.3, y: 1.3)
         
-        UIView.animate(withDuration: 5, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 6, options: .allowUserInteraction, animations: {
+        UIView.animate(withDuration: 3, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 6, options: .allowUserInteraction, animations: {
             
             self.transform = CGAffineTransform.identity
             
