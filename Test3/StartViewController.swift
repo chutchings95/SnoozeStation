a05337a1aab976adaaa40b89e557fa99fb0b554f
Chloe Hutchings <i7243787@bournemouth.ac.uk>
2017-05-27 20:28:50 +0100

    Animated start view controller buttons

--- Test3/StartViewController.swift
+++ Test3/StartViewController.swift
@@ -8,13 +8,25 @@ class StartViewController: UIViewController {
     @IBAction func selectStationButton(_ sender: Any) {
         
     }
+    
+    @IBOutlet var StnBtn: BounceButton!
+    
+    
+    @IBOutlet var favBtn: BounceButton!
   
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        StnBtn.center.x = self.view.frame.width + 30
+        favBtn.center.x = self.view.frame.height + 30
         
+        UIView.animate(withDuration: 4.0, delay: 00, usingSpringWithDamping: 1.0, initialSpringVelocity: 1.0, options: .allowAnimatedContent, animations: {
+            
+            self.StnBtn.center.y = self.view.frame.width / 2
+            self.favBtn.center.y = self.view.frame.height / 2
         
+            }, completion: nil)
         
 
         // Do any additional setup after loading the view.
