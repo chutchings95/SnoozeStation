645d9992bdb641edbdee556e5e6fc3c08ac30b0b
Chloe Hutchings <i7243787@bournemouth.ac.uk>
2017-05-28 12:51:08 +0100

    Constraints for faveButton

--- Test3/BounceButton.swift
+++ Test3/BounceButton.swift
@@ -6,7 +6,7 @@ class BounceButton: UIButton {
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         self.transform = CGAffineTransform(scaleX: 1.3, y: 1.3)
         
-        UIView.animate(withDuration: 3, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 6, options: .allowUserInteraction, animations: {
+        UIView.animate(withDuration: 1, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 6, options: .allowUserInteraction, animations: {
             
             self.transform = CGAffineTransform.identity
             
